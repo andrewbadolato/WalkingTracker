5cdd0eb2223dff15401b658514a36484bebfdc42
Andrew Badolato <21231601+andrewbadolato@users.noreply.github.com>
2018-06-10 15:48:55 -0400

    Fix text and link colors

---
+++ WalkingTracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WalkingTracker.Data;
+
+[assembly: HostingStartup(typeof(WalkingTracker.Areas.Identity.IdentityHostingStartup))]
+namespace WalkingTracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ WalkingTracker/Data/Migrations/20180610194006_NewLogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WalkingTracker.Data.Migrations
+{
+    public partial class NewLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
